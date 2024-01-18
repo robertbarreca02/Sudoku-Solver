12869e5de13135ccb6fbaab0836c28c7ec465b63
Robert Barreca <barreca@wisc.edu>
2024-01-17 18:38:10 -0600

    just getting started

--- solver.py
+++ solver.py
@@ -39,12 +39,43 @@ def extract_cols(board):
     return cols
 
 
+def backtrack(sol_board, i, j):
+    for x in range(i, -1, -1):
+        for y in range(j, -1, -1):
+            if (starter_board[x, y]) == 0:
+                insert(sol_board, sol_board[x][y] + 1, x, y)
+
+
+def insert(sol_board, starter_val, i, j):
+    # find row element is in
+    row = sol_board[i]
+    # find col element is in
+    col = cols[j]
+    # find box element is in
+    box = boxes[((i // 3) * 3) + (j // 3)]
+    for z in range(9):
+        if (z not in row) and (z not in col) and (z not in box):
+            sol_board[i][j] = z
+            return
+    # can't insert any number must backtrack
+    backtrack(sol_board, i, j)
+    return sol_board
+
+
+def solve():
+    sol_board = starter_board
+    for i in range(9):
+        for j in range(9):
+            # if num is there move to next element
+            if starter_board[i][j] != 0:
+                continue
+            sol_board = insert(sol_board, 1, i, j)
+
+
 def main():
-    # print_board(starter_board)
-    # extract_boxes(starter_board)
-    # print(boxes)
+    extract_boxes(starter_board)
     extract_cols(starter_board)
-    print(cols)
+    solve()
 
 
 if __name__ == '__main__':
