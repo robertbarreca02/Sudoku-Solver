b0e0bbf3e10624c5b322ab0e4e2187bd6b242149
Robert Barreca <barreca@wisc.edu>
2024-01-18 13:08:08 -0600

    formatting

--- solver.py
+++ solver.py
@@ -1,20 +1,29 @@
 import numpy as np
-board = np.array([
-    [0, 0, 6, 3, 9, 0, 8, 0, 1],
-    [0, 8, 9, 0, 7, 0, 5, 3, 0],
-    [3, 5, 0, 8, 0, 0, 9, 6, 0],
-    [0, 0, 0, 6, 0, 8, 0, 2, 3],
-    [8, 0, 1, 0, 3, 4, 0, 0, 0],
-    [4, 0, 3, 0, 0, 0, 6, 0, 8],
-    [6, 0, 0, 0, 0, 9, 3, 8, 0],
-    [0, 3, 0, 7, 8, 2, 0, 9, 0],
-    [0, 7, 0, 0, 0, 3, 0, 0, 0]])
+
+board = np.array(
+    [
+        [0, 0, 6, 3, 9, 0, 8, 0, 1],
+        [0, 8, 9, 0, 7, 0, 5, 3, 0],
+        [3, 5, 0, 8, 0, 0, 9, 6, 0],
+        [0, 0, 0, 6, 0, 8, 0, 2, 3],
+        [8, 0, 1, 0, 3, 4, 0, 0, 0],
+        [4, 0, 3, 0, 0, 0, 6, 0, 8],
+        [6, 0, 0, 0, 0, 9, 3, 8, 0],
+        [0, 3, 0, 7, 8, 2, 0, 9, 0],
+        [0, 7, 0, 0, 0, 3, 0, 0, 0],
+    ]
+)
 
 
 def print_board(board):
+    """
+    print_board takes in a sudoku board and prints it in sudoku format
+
+    :param board: the sudoku board (9x9 2d array) to be printed
+    """
     for i in range(9):
         if i % 3 == 0 and i != 0:
-            print("-"*21)
+            print("-" * 21)
         for j in range(9):
             if j % 3 == 0 and j != 0:
                 print("|", end=" ")
@@ -23,6 +32,16 @@ def print_board(board):
 
 
 def is_possible(i, j, n):
+    """
+    is_possible takes in an coordinate of the sudoku board and a number and checks to
+    see if the number can be inserted without breaking the rules
+
+    :param i: the y coordinate of the sudoku board
+    :param j: the x coordinate of the sudoku board
+    :param n: the number that we're trying to insert
+
+    :return: true if we can insert n legally and false otherwise
+    """
     # check if num is in row
     if n in board[i]:
         return False
@@ -40,6 +59,12 @@ def is_possible(i, j, n):
 
 
 def solve():
+    """
+    solve is a recursive function that solves the sudoku board by filling in all the zero slots of the board
+
+    :return: true if we can put a number in the first empty slot found or the board is filled and return false otherwise
+    """
+
     # base case board is filled
     if 0 not in board:
         return True
@@ -64,5 +89,5 @@ def main():
     print_board(board)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
